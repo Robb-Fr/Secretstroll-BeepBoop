9c3da7a878033ea3053b45b1058a251da0228be8
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-03-31 22:11:43 +0200

    captures

---
+++ secretstroll/capture.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "================================================================================"
+echo "Begining capture"
+echo "================================================================================"
+OUTPUT_MSG="Capture failed for traces (none if empty): "
+# we go through every cell, indexed by i
+for i in {1..100}
+    do
+        # we format the trace filename with the day-month-hour format and cell index
+        # the day-month-hour will help differentiate between different traces for same cell (but different captures)
+        # (I assume I will take 1 hour to capture for all cells)
+        trace_name=$(date +"traces/trace_%d_%m_%Hh_cell_$i.pcap")
+        echo "Capturing $trace_name ..."
+        # we capture to the file `trace_name`
+        # -s 64 saves only the first 64 bytes (contains all headers and 3 bytes of the envrypted data)
+        # greater 55 isolates packets only of length > 54 (basically removes packets having no tcp payload)
+        # tcp keeps only tcp packets to make sure we only keep traffic relevant for the application
+        tcpdump -w $trace_name -s 64 greater 55 and tcp &
+        # records the process id of the previously launched process
+        PID=$!
+        # makes sure capture is setup
+        sleep 0.1
+        # queries for cell i
+        python3 client.py grid $i -T restaurant -t
+        # we test if the query failed (returned not 0), we'll inform the user at the end
+        query_status=$?
+        if [ $query_status != 0 ]
+        then
+            OUTPUT_MSG="$OUTPUT_MSG$i "
+        fi
+        # makes sure the capture captured all
+        sleep 0.1
+        # sends SIGINT to the application
+        kill -2 $PID
+        sleep 0.1
+    done
+echo "================================================================================"
+echo $OUTPUT_MSG
+echo "================================================================================"

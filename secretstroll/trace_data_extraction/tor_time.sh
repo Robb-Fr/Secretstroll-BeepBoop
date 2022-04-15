4c52259823285e0e60dc0d06709852a9578bce40
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-04-15 12:27:25 +0200

    adds new captures

--- secretstroll/trace_data_extraction/tor_time.sh
+++ secretstroll/trace_data_extraction/tor_time.sh
@@ -9,9 +9,10 @@ echo "==========================================================================
 OUTPUT_MSG="Measure failed for trace:"
 TIMES_TOR="Measured times with tor (in seconds):"
 TIMES_NO_TOR="Measured times without tor (in seconds):"
-# we go through every cell, indexed by i
+# we go through 30 first grids, indexed by i (just enough to have a rough idea of the correlation)
 for i in {1..30}
     do
+        # !!! requires apt-get install bc (for sub second precision)
         start=$(date +%s.%N)
         # queries for cell i with tor
         python3 client.py grid $i -T restaurant -t
@@ -28,7 +29,7 @@ for i in {1..30}
             TIMES_TOR="$TIMES_TOR\n$i:$duration"
         fi
         # makes sure previous finished
-        sleep 0.1
+        sleep 1
 ###############################################################################
         start=$(date +%s.%N)
         # queries for cell i without tor
@@ -46,10 +47,11 @@ for i in {1..30}
             TIMES_NO_TOR="$TIMES_NO_TOR\n$i:$duration"
         fi
         # makes sure previous finished
-        sleep 0.1
+        sleep 1
     done
 # we get back to the folder
 cd trace_data_extraction
+# -e makes sure the \n are line return
 echo -e $TIMES_NO_TOR > times_no_tor.txt
 echo -e $TIMES_TOR > times_tor.txt
 exit 0
